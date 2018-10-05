a13d257053aeb3d779134a9661ab5c8631de753c
BuildTools <peterkjones90@gmail.com>
2018-10-04 17:21:24 -0700

    Populate valid image list and display the first image on the list to the focusImage.

--- src/pkj90/quickimagesorter/SourceManager.java
+++ src/pkj90/quickimagesorter/SourceManager.java
@@ -1,16 +1,21 @@
 package pkj90.quickimagesorter;
 
 import javafx.scene.image.Image;
+import org.apache.commons.io.FilenameUtils;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class SourceManager
 {
     private Controller controller;
     private File directory;
-    private String[] EXTENSIONS = new String[]{
+    private final String[] EXTENSIONS = new String[]{
             "png","jpg","bmp"
     };
+
+    private ArrayList<String> imageList = new ArrayList<>();
+    private ArrayList<String> deletedImages = new ArrayList<>();
 
 
 
@@ -21,14 +26,29 @@ public class SourceManager
         for (File f : directory.listFiles())
         {
             String imagePath = f.getPath();
-            System.out.println("file:"+imagePath);
-            if (imagePath.endsWith("jpg")){
-                System.out.println(f.toString());
-                cont.setFocusImage(new Image(f.toURI().toString()));
+            System.out.println("Checking file: "+imagePath);
+            if (isImage(imagePath))
+            {
+                System.out.println("Adding file: "+imagePath);
+                imageList.add(f.toURI().toString());
             }
-            else
-                System.out.println("found nothing so far");
-
         }
+
+        if (!imageList.isEmpty())
+            cont.setFocusImage(new Image(imageList.get(0)));
+    }
+
+    private boolean isImage(String image){
+        String path = FilenameUtils.getExtension(image);
+        System.out.println("Path: " + path);
+        for (String i : EXTENSIONS)
+        {
+            if (i.compareTo(path) == 0)
+            {
+                System.out.println(path + " is a match!");
+                return true;
+            }
+        }
+        return false;
     }
 }
